25b290f42faf083fef061a9cf9255fc0e3a67beb
Victor-Cordeiro <victorcordeirodesousa@gmail.com>
2025-10-01 22:02:02 -0300

    Correção no cors

--- src/main/java/locadora/configuration/CorsConfig.java
+++ src/main/java/locadora/configuration/CorsConfig.java
@@ -14,11 +14,7 @@ public class CorsConfig implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000", "http://192.168.1.210:3005",
-                                "http://186.218.63.106:3005", "http://162.240.169.65:3005",
-                                "https://www.siadfou.com.br",
-                                "http://172.18.0.3:3000", "https://siadfou.com.br", "https://www.siadfou.com.br",
-                                "https://api.siadfou.com.br", "https://seduma.siadfou.com.br/", "https://mapa.siadfou.com.br/")
+                        .allowedOrigins("http://localhost:3000", "http://192.168.1.210:3005")
                         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
